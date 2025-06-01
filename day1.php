181da9d27db7c8cae08b7a527abad646dd7db028
Himadry Mondol <himadrymondol81@gmail.com>
2025-06-01 23:40:30 +0600

    im doing php at the same time

---
+++ day1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <form action="">
+        <div><label for="login">Profile name:<input type="text"name="username"></label>
+        </div>
+        <label for="">password:<input type="password"></label>
+    </form>
+    <form action="radio">
+        <h3>what is Mahadi Mustuba</h3>
+        <label for=""><div><input type="radio">Ram chagol</div><div> <input type="radio">Dencerious bici</div>
+        <div><input type="radio">Badore Cusha am er ati</div>
+    </label>
+    </form>
+</body> 
---
+++ hellow.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+<?php
+$hard=true;
+echo"she will propose me status: {$hard}"
+?>
+</h1>
+</body>
+</html>
+
